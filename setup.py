ff7f4a0511168607d23b78bd0ca2540961365d7b
Guy Smoilovsky <611655+guysmoilov@users.noreply.github.com>
2020-06-16 10:59:35 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dagshub",
-    version="0.0.2rc1",
+    version="0.1.0",
     author="DAGsHub",
     author_email="contact@dagshub.com",
     description="DAGsHub client libraries",
