54a593e6922e333e48f183156dec8f93d9094612
Guy Smoilovsky <611655+guysmoilov@users.noreply.github.com>
2020-09-10 16:03:55 +0300

    Bump version to 0.1.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dagshub",
-    version="0.1.3",
+    version="0.1.4",
     author="DAGsHub",
     author_email="contact@dagshub.com",
     description="DAGsHub client libraries",
