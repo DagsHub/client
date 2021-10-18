dc48f10aef7dfef23f1186f8e56d23aba3b8f3be
Arjun Vikram <arjvik@gmail.com>
2021-10-18 08:58:03 -0500

    Keras callback (#130)
    * Introduce keras callback contribution
    
    * Add keras callback code
    
    * Add keras callback example code to test and demonstrate functionality
    
    * Ignore generated metrics.csv and params.yml files from example code
    
    * Add example to keras callback documentation
    
    * Update README.md
    
    Co-authored-by: Guy Smoilovsky <611655+guysmoilov@users.noreply.github.com>
    
    * Update dagshub/keras/logger.py
    
    Co-authored-by: Guy Smoilovsky <611655+guysmoilov@users.noreply.github.com>
    
    * Add on_train_end callback to close logger object
    
    Fixes https://github.com/DAGsHub/client/pull/130#discussion_r730427564
    
    * Log success hyperparameter when model training completes
    
    Fixes https://github.com/DAGsHub/client/pull/130#discussion_r730426124
    
    * Move optimizer hyperparameters to their own key 'optimizer'
    
    Fixes https://github.com/DAGsHub/client/pull/130#discussion_r730426382
    
    * Prevent incrementing optimizer step number at epoch end
    
    Fixes https://github.com/DAGsHub/client/pull/130#discussion_r730426737
    
    Co-authored-by: Guy Smoilovsky <611655+guysmoilov@users.noreply.github.com>

---
+++ dagshub/keras/logger.py
@@ -0,0 +1,82 @@
+from tensorflow.keras.callbacks import Callback
+
+from ..logger import DAGsHubLogger as LoggerImpl
+
+class ignore_exceptions:
+    #Taken from fastcore code
+    "Context manager to ignore exceptions"
+    def __enter__(self): pass
+    def __exit__(self, *args): return True
+
+class DAGsHubLogger(Callback):
+    """
+    First, install the DAGsHub logger with
+    `pip install dagshub`
+
+    Example of Usage:
+    ```
+    from dagshub.keras import DAGsHubLogger
+    from tensorflow.keras import Trainer
+
+    model = ...
+
+    model.fit(
+        dataset,
+        ...,
+        callbacks=[DAGsHubLogger()]
+    )
+    ```
+    """
+
+    def __init__(self,
+                 metrics_path: str = 'metrics.csv',
+                 should_log_metrics: bool = True,
+                 hparams_path: str = 'params.yml',
+                 should_log_hparams: bool = True,
+                 should_make_dirs: bool = True,
+                 ):
+        """
+        :param metrics_path (str): Where to save the single metrics CSV file.
+        :param should_log_metrics (bool): Whether to log metrics at all. Should probably always be True.
+        :param hparams_path (str): Where to save the single hyperparameter YAML file.
+        :param should_log_hparams (bool): Whether to log hyperparameters to a file.
+            Should be False if you want to work with hyperparameters in a dependency file,
+            rather than specifying them using command line arguments.
+        :param should_make_dirs (bool): If true, the directory structure required by metrics_path
+            and hparams_path will be created. Has no effect if the directory structure already exists.
+        """
+        super(DAGsHubLogger, self).__init__()
+        self.logger = LoggerImpl(metrics_path=metrics_path, should_log_metrics=should_log_metrics,
+                                 hparams_path=hparams_path, should_log_hparams=should_log_hparams,
+                                 should_make_dirs=should_make_dirs, eager_logging=True)
+
+    def on_train_begin(self, logs={}):
+        params = {}
+        with ignore_exceptions():
+            params.update(self.params)
+        with ignore_exceptions():
+            params['optimizer'] = self.model.optimizer.get_config()
+        with ignore_exceptions():
+            params['loss'] = self.model.loss.get_config()
+        self.logger.log_hyperparams(params)
+        self.logger.log_hyperparams(success=False)
+        self._epoch = -1
+        self._step = 0
+
+    def on_epoch_begin(self, epoch, logs={}):
+        self._epoch = epoch
+
+    def on_train_batch_end(self, batch, logs={}):
+        self._step += 1
+        self.on_epoch_end(None, logs)
+    
+    def on_epoch_end(self, epoch, logs={}):
+        # At the end of an epoch, logs has more metrics
+        metrics = {'epoch': self._epoch, **logs}
+        self.logger.log_metrics(metrics, step_num=self._step)
+
+    def on_train_end(self, logs={}):
+        self.logger.log_hyperparams(success=True)
+        self.logger.save()
+        self.logger.close()
+    
---
+++ examples/keras/mnist.py
@@ -0,0 +1,25 @@
+# Credit to TensorFlow Expert Quickstart for providing much of this code
+# Note: This is a terrible model architecture with bad pracices, meant only to demonstrate and test the DAGsHubLogger callback
+from dagshub.keras import DAGsHubLogger
+
+import tensorflow as tf
+print("TensorFlow version:", tf.__version__)
+
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, Input
+from tensorflow.keras import Sequential
+from tensorflow.keras.datasets import mnist
+from tensorflow.data import Dataset
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+x_train = x_train[..., tf.newaxis].astype("float32")
+x_test = x_test[..., tf.newaxis].astype("float32")
+train_ds = Dataset.from_tensor_slices((x_train, y_train)).shuffle(10000).batch(32)
+test_ds = Dataset.from_tensor_slices((x_test, y_test)).batch(32)
+
+
+model = Sequential([Input((28, 28, 1)), Conv2D(10, 3, activation='relu'), Flatten(), Dense(20, activation='relu'), Dense(10)])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics='accuracy')
+model.summary();
+
+model.fit(train_ds, validation_data=test_ds, epochs=10, callbacks=[DAGsHubLogger()])
