5e9eec501889dfefbd6340c03edf6c3f2ad27e3f
Guy Smoilovsky <611655+guysmoilov@users.noreply.github.com>
2021-10-18 19:00:09 +0300

    Bump version to 0.1.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dagshub",
-    version="0.1.7",
+    version="0.1.8",
     author="DAGsHub",
     author_email="contact@dagshub.com",
     description="DAGsHub client libraries",
