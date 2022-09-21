7a7efa9bc51d4d6235574ea7ff4f22359e2a85d7
Ido Nov <idonov8@gmail.com>
2022-09-21 18:02:10 +0300

    remove redundant comments

--- upload-wrapper/test.py
+++ upload-wrapper/test.py
@@ -1,16 +1,9 @@
 import dagshub
 
-# Simon's idea of it:
+# A basic use case
 repo = dagshub.Repo("idonov8", "test-all-file-kinds")
 ds = repo.directory("path/to/ds")
-# if not ds.exists() -> ds.create() // for now, throw exeption
 
-# Directory either exists or will be created
-# print(ds.exists())
-# "True" or "False"
 with open("test_photo.png", 'rb') as f:
 	ds.add(file=f, path="new_folder")
 	ds.commit("Add a photo", versioning="dvc")
-
-# for f in files:
-#     ds.add(file=f, path="rel/path")
