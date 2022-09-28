7bd5fc65174cda56358c5e812152421d833bb89f
Ido Nov <idonov8@gmail.com>
2022-09-28 12:42:04 +0300

    CR fixes

--- dagshub/upload/wrapper.py
+++ dagshub/upload/wrapper.py
@@ -7,52 +7,53 @@ DEFAULT_SOURCE_URL = "https://dagshub.com/"
 CONTENT_UPLOAD_URL = "api/v1/repos/{owner}/{reponame}/content/{branch}/{path}"
 REPO_INFO_URL = "api/v1/repos/{owner}/{reponame}"
 
+def get_default_branch(src_url, owner, reponame):
+	res = requests.get(urllib.parse.urljoin(src_url, REPO_INFO_URL.format(
+		owner=owner,
+		reponame=reponame
+		)))
+	return res.json().get('default_branch')
+
 class Repo:
 	src_url = DEFAULT_SOURCE_URL
 	def __init__(self, owner, name, authToken=None, src_url=None, branch=None):
 		self.owner = owner
 		self.name = name
+		self.src_url = src_url if src_url is not None else os.environ.get("SRC_URL")
 
-		if authToken != None:
+		if authToken is not None:
 			self.authToken = authToken
 		elif "ACCESS_TOKEN" in os.environ:
 			self.authToken = os.environ["ACCESS_TOKEN"]
 		else:
 			raise Exception("Can't find access token. Please set enviroment variable ACCESS_TOKEN with a DagsHub access token")
 		# TODO: verify token
-		
-		if src_url != None:
-			self.src_url = src_url
-		elif "SRC_URL" in os.environ:
-			self.src_url = os.environ["SRC_URL"]
 
-		if branch != None:
+		if branch is not None:
 			self.branch = branch
 		else:
-			# Find default branch and set it if not specified
-			res = requests.get(urllib.parse.urljoin(self.src_url, REPO_INFO_URL.format(
-				owner=self.owner,
-				reponame=self.name
-				)))
-			try:
-				self.branch = res.json().get('default_branch')
-				print("Set default branch: ", self.branch)
-			except:
-				raise Exception("Failed to get default branch for repository. Please specify a branch and make sure repository details are correct.")
-
+			print("Branch wasn't provided. Fetching default branch.")
+			self._set_default_branch()
+		print("Set branch: ", self.branch)
 
 	def directory(self, path):
 		return DataSet(self, path)
+		
+
+	def _set_default_branch(self):
+		try:
+			self.branch = get_default_branch(self.src_url, self.owner, self.name)
+		except:
+			raise Exception("Failed to get default branch for repository. Please specify a branch and make sure repository details are correct.")
 
 class Commit:
-	choice="direct"
-	message=""
-	summary=""
-	versioning="auto"
-	new_branch=""
-	last_commit=""
 	def __init__(self):
-		return
+		self.choice="direct"
+		self.message=""
+		self.summary=""
+		self.versioning="auto"
+		self.new_branch=""
+		self.last_commit=""
 
 class DataSet:
 	directory = ""
@@ -70,7 +71,7 @@ class DataSet:
 
 	def add(self, file, path=".", target_dir=None):
 		# path is the full target path, including the file name
-		if target_dir != None:
+		if target_dir is not None:
 			if path != ".":
 				raise Exception("You must provide either a path or a target_dir. You can't provide both")
 			path = os.path.join(target_dir, os.path.basename(os.path.normpath(file if type(file) is str else file.name)))
@@ -87,15 +88,15 @@ class DataSet:
 
 	def commit(self, message, versioning=None, new_branch=None, last_commit=None):
 		data = {}
-		if versioning != None:
+		if versioning is not None:
 			self.commit_data.versioning = versioning
 		data["versioning"] = self.commit_data.versioning
 
-		if last_commit != None:
+		if last_commit is not None:
 			self.commit_data.last_commit = last_commit
 			data["last_commit"] = self.commit_data.last_commit
 
-		if new_branch != None:
+		if new_branch is not None:
 			self.commit_data.choice = "commit-to-new-branch"
 			self.commit_data.new_branch = new_branch
 
@@ -124,3 +125,6 @@ class DataSet:
 			headers={'Authorization': 'token ' + self.repo.authToken})
 		print("Response: ", res.status_code)
 		pprint(res.content)
+
+		if res.status_code == 200:
+			print("Upload finished successfully!")
