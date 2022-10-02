5ff115978fb07c5303ce0a720d3657bbdbf52384
Ido Nov <idonov8@gmail.com>
2022-10-02 12:58:29 +0300

    remove OAuth for now

--- dagshub/upload/wrapper.py
+++ dagshub/upload/wrapper.py
@@ -1,7 +1,6 @@
 import requests
 import urllib
 import os
-from dagshub.auth import get_oauth_token
 from pprint import pprint
 from typing import Union
 from io import IOBase
@@ -28,10 +27,7 @@ class Repo:
 		elif "ACCESS_TOKEN" in os.environ:
 			self.authToken = os.environ["ACCESS_TOKEN"]
 		else:
-			try:
-				self.authToken = get_oauth_token(host=self.src_url)
-			except:
-				raise Exception("\n --- OAuth failed, try again --- \n Altenatively, you can set an enviroment variable ACCESS_TOKEN with a DagsHub access token or pass it to Repo with 'authToken' instead.")
+			raise Exception("Can't find access token. You can set an enviroment variable ACCESS_TOKEN with a DagsHub access token or pass it to Repo with 'authToken'.")
 		# TODO: verify token
 
 		if branch is not None:
