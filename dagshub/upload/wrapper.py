27ed2cc8930cce8465f2dd2cece3df380a5cfa4d
Ido Nov <idonov8@gmail.com>
2022-10-02 17:46:33 +0300

    Add username&password instead of token + debug logger

--- dagshub/upload/wrapper.py
+++ dagshub/upload/wrapper.py
@@ -4,6 +4,7 @@ import os
 from pprint import pprint
 from typing import Union
 from io import IOBase
+from dagshub.upload.debug_logger import logger
 
 DEFAULT_SOURCE_URL = "https://dagshub.com/"
 CONTENT_UPLOAD_URL = "api/v1/repos/{owner}/{reponame}/content/{branch}/{path}"
@@ -17,25 +18,37 @@ def get_default_branch(src_url, owner, reponame):
 	return res.json().get('default_branch')
 
 class Repo:
-	def __init__(self, owner, name, authToken=None, src_url=None, branch=None):
-		self.owner = owner
-		self.name = name
-		self.src_url = src_url if src_url is not None else os.environ.get("SRC_URL") if "SRC_URL" in os.environ else DEFAULT_SOURCE_URL
+	def __init__(self, owner, name, username=None, password=None, src_url=None, branch=None):
+		try:
+			self.owner = owner
+			self.name = name
+			self.src_url = src_url if src_url is not None else os.environ.get("SRC_URL") if "SRC_URL" in os.environ else DEFAULT_SOURCE_URL
 
-		if authToken is not None:
-			self.authToken = authToken
-		elif "ACCESS_TOKEN" in os.environ:
-			self.authToken = os.environ["ACCESS_TOKEN"]
-		else:
-			raise Exception("Can't find access token. You can set an enviroment variable ACCESS_TOKEN with a DagsHub access token or pass it to Repo with 'authToken'.")
-		# TODO: verify token
+			if username is not None:
+				self.username = username
+			elif "DAGSHUB_USERNAME" in os.environ:
+				self.username = os.environ["DAGSHUB_USERNAME"]
+			else:
+				logger.warning("No DagsHub username specified, defaulting to repo owner. If you're not the owner of the repository you're working on, please speciy your username.")
+			self.username = owner
+			if password is not None:
+				self.password = password
+			elif "DAGSHUB_PASSWORD" in os.environ:
+				self.password = os.environ["DAGSHUB_PASSWORD"]
+			else:
+				raise Exception("Can't find a password/access token. You can set an enviroment variable DAGSHUB_PASSWORD with it or pass it to Repo with 'password'.")
+				# TODO: verify token
 
-		if branch is not None:
-			self.branch = branch
-		else:
-			print("Branch wasn't provided. Fetching default branch...")
-			self._set_default_branch()
-		print("Set branch: ", self.branch)
+			if branch is not None:
+				self.branch = branch
+			else:
+				print("Branch wasn't provided. Fetching default branch...")
+				self._set_default_branch()
+			print("Set branch: ", self.branch)
+
+		except Exception as e:
+			logger.error(e)
+			quit()
 
 	def upload(self, file: Union[str, IOBase], message, versioning=None, new_branch=None, last_commit=None, path=None):
 		ds = DataSet(self, ".")
@@ -63,78 +76,88 @@ class Repo:
 class Commit:
 	def __init__(self):
 		self.choice="direct"
-		self.message=""
-		self.summary=""
+		self.message=None
+		self.summary=None
 		self.versioning="auto"
-		self.new_branch=""
-		self.last_commit=""
+		self.new_branch=None
+		self.last_commit=None
 
 class DataSet:
 	files = []
 	commit_data = Commit()
-	def __init__(self, repo, directory):
+	def __init__(self, repo: Repo, directory):
 		self.repo = repo
 		self.directory = directory
 		self.request_url = self.repo.get_request_url(directory)
 
 	def add(self, file: Union[str, IOBase], path=None):
-		# if path is not provided, fall back to the file name
-		if path is None:
-			try:
-				path = os.path.basename(os.path.normpath(file if type(file) is str else file.name))
-			except:
-				raise Exception("Could not interpret your file's name. Please specify it in the keyword parameter 'path'.")
+		try:
+			# if path is not provided, fall back to the file name
+			if path is None:
+				try:
+					path = os.path.basename(os.path.normpath(file if type(file) is str else file.name))
+				except:
+					raise Exception("Could not interpret your file's name. Please specify it in the keyword parameter 'path'.")
 
-		if type(file) is str:
-			try:
-				f = open(file, 'rb')
-				self.files.append((path, f))
-				return
-			except IsADirectoryError:
-				raise IsADirectoryError("'file' must describe a file, not a directory.")
+			if type(file) is str:
+				try:
+					f = open(file, 'rb')
+					self.files.append((path, f))
+					return
+				except IsADirectoryError:
+					raise IsADirectoryError("'file' must describe a file, not a directory.")
 
-		self.files.append((path, file))
+			self.files.append((path, file))
+
+		except Exception as e:
+			logger.error(e)
+			quit()
 
 	def commit(self, message, versioning=None, new_branch=None, last_commit=None):
-		data = {}
-		if versioning is not None:
-			self.commit_data.versioning = versioning
-		data["versioning"] = self.commit_data.versioning
+		try:
+			data = {}
+			if versioning is not None:
+				self.commit_data.versioning = versioning
+			data["versioning"] = self.commit_data.versioning
 
-		if last_commit is not None:
-			self.commit_data.last_commit = last_commit
-			data["last_commit"] = self.commit_data.last_commit
+			if last_commit is not None:
+				self.commit_data.last_commit = last_commit
+				data["last_commit"] = self.commit_data.last_commit
 
-		if new_branch is not None:
-			self.commit_data.choice = "commit-to-new-branch"
-			self.commit_data.new_branch = new_branch
+			if new_branch is not None:
+				self.commit_data.choice = "commit-to-new-branch"
+				self.commit_data.new_branch = new_branch
 
-		data["commit_choice"] = self.commit_data.choice
-		
-		if self.commit_data.choice == "commit-to-new-branch":
-			data["new_branch_name"] = self.commit_data.new_branch
-		
-		if message != "":
-			self.commit_data.message = message
-		else:
-			raise Exception("You must provide a valid commit message")
-		data["commit_message"] = self.commit_data.message
+			data["commit_choice"] = self.commit_data.choice
+			
+			if self.commit_data.choice == "commit-to-new-branch":
+				data["new_branch_name"] = self.commit_data.new_branch
+			
+			if message != "":
+				self.commit_data.message = message
+			else:
+				raise Exception("You must provide a valid commit message")
+			data["commit_message"] = self.commit_data.message
 
-		# Prints for debugging
-		print("URL: ", self.request_url)
-		print("DATA:")
-		pprint(data)
-		print("Files:")
-		pprint(self.files)
-		print("making request...")
-		res = requests.put(
-			self.request_url, 
-			data, 
-			params={"is_dvc_dir": True},
-			files=[("files", file) for file in self.files],
-			headers={'Authorization': 'token ' + self.repo.authToken})
-		print("Response: ", res.status_code)
-		pprint(res.content)
+			# Prints for debugging
+			print("URL: ", self.request_url)
+			print("DATA:")
+			pprint(data)
+			print("Files:")
+			pprint(self.files)
+			print("making request...")
+			res = requests.put(
+				self.request_url, 
+				data, 
+				params={"is_dvc_dir": True},
+				files=[("files", file) for file in self.files],
+				auth=(self.repo.username, self.repo.password ))
+			print("Response: ", res.status_code)
+			pprint(res.content)
 
-		if res.status_code == 200:
-			print("Upload finished successfully!")
+			if res.status_code == 200:
+				print("Upload finished successfully!")
+
+		except Exception as e:
+			logger.error(e)
+			quit()
