7a27e5239b849ad8f0fd920fe846bc09b4b4c967
Simon Lousky <simon@dagshub.com>
2022-10-06 17:04:33 +0300

    include scheme in host

--- dagshub/common/config.py
+++ dagshub/common/config.py
@@ -1,8 +1,9 @@
 import appdirs
 import os
+from urllib.parse import urlparse
 
 HOST_KEY = "DAGSHUB_CLIENT_HOST"
-DEFAULT_HOST = "dagshub.com"
+DEFAULT_HOST = "https://dagshub.com"
 CLIENT_ID_KEY = "DAGSHUB_CLIENT_ID"
 DEFAULT_CLIENT_ID = "32b60ba385aa7cecf24046d8195a71c07dd345d9657977863b52e7748e0f0f28"
 TOKENS_CACHE_LOCATION_KEY = "DAGSHUB_CLIENT_TOKENS_CACHE"
@@ -12,8 +13,9 @@ DEFAULT_TOKENS_CACHE_LOCATION = os.path.join(
 TOKENS_CACHE_SCHEMA_VERSION = "1"
 DAGSHUB_USER_TOKEN_KEY = "DAGSHUB_USER_TOKEN"
 
-hostname = os.environ.get(HOST_KEY, DEFAULT_HOST)
-host = f"https://{hostname}"
+parsed_host = urlparse(os.environ.get(HOST_KEY, DEFAULT_HOST))
+hostname = parsed_host.hostname
+host = parsed_host.geturl()
 client_id = os.environ.get(CLIENT_ID_KEY, DEFAULT_CLIENT_ID)
 cache_location = os.environ.get(
     TOKENS_CACHE_LOCATION_KEY, DEFAULT_TOKENS_CACHE_LOCATION
