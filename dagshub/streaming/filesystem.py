bda868460d555369739651c17eb09b2fdd0000c0
KBolashev <kirill@dagshub.com>
2022-10-16 10:07:37 +0300

    Remove unused import

--- dagshub/streaming/filesystem.py
+++ dagshub/streaming/filesystem.py
@@ -3,7 +3,6 @@ import io
 import os
 import re
 import subprocess
-import sys
 from configparser import ConfigParser
 from contextlib import contextmanager
 from functools import partial, wraps, lru_cache
