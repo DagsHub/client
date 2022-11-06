b114f8f84d2fd29cf5a19daa37571f82847445d5
KBolashev <kirill@dagshub.com>
2022-11-06 17:13:31 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf8") as fh:
 
 setuptools.setup(
     name="dagshub",
-    version="0.2.0",
+    version="0.2.1",
     author="DagsHub",
     author_email="contact@dagshub.com",
     description="DagsHub client libraries",
