b0f214a75733fa76cf19ed0f3b14f932a8c8d466
Martin Daniel <martin@dagshub.com>
2022-11-06 12:10:00 +0200

    Add function to add a whole directory to a remote repo

--- dagshub/upload/wrapper.py
+++ dagshub/upload/wrapper.py
@@ -170,18 +170,6 @@ class Repo:
     def directory(self, path):
         return DataSet(self, path)
 
-    def upload_directory(self, remote_path, local_path, glob_exclude=""):
-        ds = self.directory(local_path)
-        for root, dirs, files in os.walk(local_path):
-            if len(files) > 0:
-                for filename in files:
-                    rel_file_path = os.path.join(root, filename)
-                    ds.add(file=rel_file_path, path=remote_path)
-                commit_message = "Commit data points in folder %s" % root
-                print(commit_message)
-                print(ds.files)
-                ds.commit(commit_message, versioning="dvc")
-
     def get_request_url(self, directory):
         return urllib.parse.urljoin(self.src_url, CONTENT_UPLOAD_URL.format(
             owner=self.owner,
@@ -216,6 +204,16 @@ class DataSet:
             if path in self.files:
                 logger.warning(f"File already staged for upload on path \"{path}\". Overwriting")
             self.files[path] = (path, file)
+
+    def add_dir(self, local_path, glob_exclude=""):
+        for root, dirs, files in os.walk(local_path):
+            if len(files) > 0:
+                for filename in files:
+                    rel_file_path = os.path.join(root, filename)
+                    rel_remote_file_path = rel_file_path.replace(local_path, "")
+                    self.add(file=rel_file_path, path=rel_remote_file_path)
+                commit_message = "Commit data points in folder %s" % root
+                self.commit(commit_message, versioning="dvc")
 
     @staticmethod
     def _clean_directory_name(directory: str):
