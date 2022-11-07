6e1d834c1bec1f04dc9ae58d2eafa7b85b0511cc
KBolashev <kirill@dagshub.com>
2022-11-07 10:05:40 +0200

    Moved __version__ to __init__.py Setup.py now imports the version by reading the file

--- setup.py
+++ setup.py
@@ -1,11 +1,32 @@
 import setuptools
+import os.path
+
+
+# Thank you pip contributors
+def read(rel_path: str) -> str:
+    here = os.path.abspath(os.path.dirname(__file__))
+    # intentionally *not* adding an encoding option to open, See:
+    #   https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
+    with open(os.path.join(here, rel_path)) as fp:
+        return fp.read()
+
+
+def get_version(rel_path: str) -> str:
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            # __version__ = "0.9"
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    raise RuntimeError("Unable to find version string.")
+
 
 with open("README.md", "r", encoding="utf8") as fh:
     long_description = fh.read()
 
+
 setuptools.setup(
     name="dagshub",
-    version="0.2.1",
+    version=get_version("dagshub/__init__.py"),
     author="DagsHub",
     author_email="contact@dagshub.com",
     description="DagsHub client libraries",
