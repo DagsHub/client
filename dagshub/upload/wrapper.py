92e1d22e653e6fdcd7c0c74c535d8d3dad0142c2
Krishna Dutt Panchagnula <krishnadutt123@gmail.com>
2022-11-16 21:45:09 +0530

    Update dagshub/upload/wrapper.py
    Co-authored-by: Ido Nov <idonov8@gmail.com>

--- dagshub/upload/wrapper.py
+++ dagshub/upload/wrapper.py
@@ -242,7 +242,7 @@ class Repo:
     @property
     def auth(self):
         """
-        The auth function is used to authenticate the user with the dagshub API.It takes in a username and password, or token as arguments. If both are provided, it will use the username and password combination to
+        The auth function is used to authenticate the user with the dagshub API. It takes in a username and password, or token as arguments. If both are provided, it will use the username and password combination to
                  get a token from dagshub's authentication server. Otherwise, if only a token is provided it will be used for authentication.
         
         :return: The httpbasicauth object
