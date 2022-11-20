ca20f9a8accbf4fa86fac95d519ec1e88d42f99b
KBolashev <kirill@dagshub.com>
2022-11-20 18:19:52 +0200

    Proper auth skipping in tests

--- tests/dda/filesystem/conftest.py
+++ tests/dda/filesystem/conftest.py
@@ -4,12 +4,6 @@ import pytest
 import dagshub
 
 
-@pytest.fixture(autouse=True)
-def mock_env_vars():
-    # Skip auth
-    os.environ["DAGSHUB_USER_TOKEN"] = "token"
-
-
 @pytest.fixture
 def repo_with_hooks(dagshub_repo):
     dagshub.streaming.install_hooks()
