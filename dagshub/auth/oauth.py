8062ac8d4e1204884ca62c27f286dfa18eaab774
Tal Malka <tal@dagshub.com>
2022-11-21 12:32:07 +0200

    fixing review comments

--- dagshub/auth/oauth.py
+++ dagshub/auth/oauth.py
@@ -4,16 +4,8 @@ import requests
 import urllib
 import uuid
 from dagshub.common import config
-import concurrent.futures
+
 logger = logging.getLogger(__name__)
-
-
-def call_middleman(dagshub_url, middle_man_uuid):
-    res = requests.post(
-        f"{dagshub_url}/middleman",
-        data={"uuid": middle_man_uuid},
-    )
-    return res
 
 
 def oauth_flow(
@@ -27,20 +19,20 @@ def oauth_flow(
     state = uuid.uuid4()
     middle_man_uuid = uuid.uuid4()
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        future = executor.submit(call_middleman, dagshub_url, middle_man_uuid)
-        link_prompt = f"Go to {dagshub_url}/authorize?state={state}&client_id={client_id}" \
-                      f"&middleman_uuid={middle_man_uuid} to authorize the client."
-        print(link_prompt)
-        res = future.result()
+    link_prompt = f"Go to {dagshub_url}/authorize?state={state}&client_id={client_id}" \
+                  f"&middleman_uuid={middle_man_uuid} to authorize the client."
+    print(link_prompt)
+
+    res = requests.post(
+        f"{dagshub_url}/middleman",
+        data={"uuid": middle_man_uuid},
+    )
 
     if res.status_code != 200:
-        # maybe add a different error for the timeout flow
         raise Exception(
             f"Error while getting OAuth code: HTTP {res.status_code}, Body: {res.json()}"
         )
     code = res.json()["Code"]
-    print(code)
 
     res = requests.post(
         f"{dagshub_url}/access_token",
@@ -53,7 +45,6 @@ def oauth_flow(
     token = res.json()
 
     logger.debug(f"Got token: {token}")
-    # Guy- should we print so massage to let the user know he is now logged in?
     return token
 
 
