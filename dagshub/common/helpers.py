88f94c05b5fccb753cc0495b071815ea5c19bc54
Jinen Setpal <jinens8@gmail.com>
2022-12-28 13:06:32 -0400

    messy but functional

--- dagshub/common/helpers.py
+++ dagshub/common/helpers.py
@@ -1,6 +1,7 @@
 import httpx
 
 from ..upload.wrapper import create_repo
+from dagshub.auth import get_token
 from dagshub.common import config
 from pathlib import Path
 import shutil
@@ -35,7 +36,7 @@ def http_request(method, url, **kwargs):
             kwargs[arg] = mixin_args[arg]
     return httpx.request(method, url, **kwargs)
 
-def init(repo_name, repo_owner, host='https://dagshub.com/'):
+def init(repo_name, repo_owner, host=config.DEFAULT_HOST):
     import dagshub.auth
     from dagshub.auth.token_auth import HTTPBearerAuth
 
@@ -44,7 +45,7 @@ def init(repo_name, repo_owner, host='https://dagshub.com/'):
     if username is not None and password is not None:
         auth = username, password
     else:
-        token = config.token or dagshub.auth.get_token()
+        token = config.token or get_token()
         if token is not None:
             auth = token, token
             bearer = HTTPBearerAuth(token)
@@ -53,7 +54,7 @@ def init(repo_name, repo_owner, host='https://dagshub.com/'):
     res = http_request("GET", urllib.parse.urljoin(host, config.REPO_INFO_URL.format(
         owner=repo_owner,
         reponame=repo_name)), auth=bearer or auth)
-    if res.getcode() == 404: create_repo(repo_name, repo_owner)
+    if res.status_code == 404: create_repo(repo_name)
 
     # MLFlow environment variables
     os.environ['MLFLOW_TRACKING_URI'] = f'{uri}.mlflow'
@@ -61,12 +62,22 @@ def init(repo_name, repo_owner, host='https://dagshub.com/'):
     os.environ['MLFLOW_TRACKING_PASSWORD'] = auth[1]
 
     # DVC
-    res = http_request("GET", urllib.parse.urljoin(host, config.REPO_INFO_URL.format(
-        owner=repo_owner,
-        reponame=repo_name)), auth=bearer or auth)
-    conf_path = Path(__file__).parent.parent.as_posix(), 'etc', 'config'),
-    shutil.copyfile(os.path.join(Path(__file__).parent.parent.as_posix(), 'etc', 'config.template'), conf_path)
+    # res = http_request("GET", urllib.parse.urljoin(host, config.REPO_RAW_URL.format(
+    #     owner=repo_owner,
+    #     reponame=repo_name)), auth=bearer or auth)
+    # conf_path = os.path.join(Path(__file__).parent.parent.as_posix(), 'etc', 'config')
+    # shutil.copyfile(os.path.join(Path(__file__).parent.parent.as_posix(), 'etc', 'config.template'), conf_path)
 
-    with open(conf_path, 'a') as conf: conf.write(f'[\'remote "{remote}"\']\n    url = {uri}.dvc')
+    # flag = False
+    # remote = 'origin'
+    # with open(conf_path, 'ra') as conf:
+    #     for line in conf.readlines():
+    #         if remote in line: flag = True
+    #         if flag and 'dagshub' not in line: 
+    #             remote = 'dagshub'
+    #             flag = False
+    #     if not flag:
+    #         conf.write(f'[\'remote "{remote}"\']\n    url = {uri}.dvc\n')
+    #         print(f'Added new remote "{remote}" with url = {uri}')
 
     print('Repository initialized!')
