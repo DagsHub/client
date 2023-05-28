941bf87d9f63312eb6a34d3e917ed5b636afd70e
KBolashev <kirill@dagshub.com>
2023-05-28 16:03:18 +0300

    Fix for multiple calls to `to_voxel51_dataset`

--- dagshub/data_engine/model/datasource.py
+++ dagshub/data_engine/model/datasource.py
@@ -268,8 +268,9 @@ class Datasource:
         script_dir = os.path.dirname(__file__)
         plugin_dir = os.environ.get("DAGSHUB_FIFTYONE_PLUGINS_DIR",
                                     os.path.join(script_dir, "../voxel_plugin_server/plugins"))
+        plugin_dir = os.path.abspath(plugin_dir)
         fo_dir_envkey = "FIFTYONE_PLUGINS_DIR"
-        if fo_dir_envkey in os.environ:
+        if fo_dir_envkey in os.environ and os.environ[fo_dir_envkey] != plugin_dir:
             plugins_dest_dir = os.path.join(os.environ[fo_dir_envkey], "dagshub")
             # TODO: handle version changes, for now prompt only if the plugin isn't copied
             if not os.path.exists(plugins_dest_dir):
