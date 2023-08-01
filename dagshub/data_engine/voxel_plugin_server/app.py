c721265fa02ca3332d330507fc853ef87a4efe85
KBolashev <kirill@dagshub.com>
2023-08-01 17:54:17 +0300

    Add a route to refresh the voxel dataset

--- dagshub/data_engine/voxel_plugin_server/app.py
+++ dagshub/data_engine/voxel_plugin_server/app.py
@@ -6,7 +6,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import JSONResponse
 from starlette.routing import Route
 
-from dagshub.data_engine.voxel_plugin_server.routes.datasource import get_fields, update_metadata
+from dagshub.data_engine.voxel_plugin_server.routes.datasource import get_fields, update_metadata, refresh_dataset
 from dagshub.data_engine.voxel_plugin_server.routes.annotation import to_annotate
 from dagshub.data_engine.voxel_plugin_server.routes.voxel import save_dataset
 
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 
 
 async def homepage(request):
-    return JSONResponse({"Hello": "from the dagshub voxel dagshub server"})
+    return JSONResponse({"Hello": "from the dagshub voxel plugin server"})
 
 
 app = Starlette(debug=True,
@@ -28,7 +28,8 @@ app = Starlette(debug=True,
                 routes=[
                     Route("/", homepage),
                     Route("/labelstudio/", to_annotate, methods=["POST"]),
-                    Route("/save_dataset/", save_dataset, methods=["POST"]),
+                    Route("/dataset/save", save_dataset, methods=["POST"]),
+                    Route("/dataset/refresh", refresh_dataset),
                     Route("/datasource/fields", get_fields),
                     Route("/datasource/update_metadata", update_metadata, methods=["POST"]),
                 ])
