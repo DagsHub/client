073a614965d38848ce65389c7806cadfe52a8bbf
KBolashev <kirill@dagshub.com>
2023-11-12 15:22:17 +0200

    Initial implementation of the get_repo_bucket_client function

--- dagshub/__init__.py
+++ dagshub/__init__.py
@@ -3,6 +3,7 @@ from .logger import DAGsHubLogger, dagshub_logger
 from .common.init import init
 from .upload.wrapper import upload_files
 from . import notebook
+from .repo_bucket import get_repo_bucket_client
 
 __all__ = [
     DAGsHubLogger,
@@ -10,4 +11,5 @@ __all__ = [
     init,
     upload_files,
     notebook.save_notebook,
+    get_repo_bucket_client,
 ]
