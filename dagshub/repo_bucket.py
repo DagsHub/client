789025c1d0c98b6e7ea86cd6799bb18c41c98e29
KBolashev <kirill@dagshub.com>
2023-11-16 15:30:04 +0200

    Add docs, switch the print to a log_message

--- dagshub/repo_bucket.py
+++ dagshub/repo_bucket.py
@@ -1,22 +1,31 @@
 from dagshub.common.api.repo import RepoAPI
 from dagshub.auth import get_token
+from dagshub.common.helpers import log_message
 
 
 def get_boto_client(repo_api: RepoAPI, token: str):
+    """
+    Creates a `boto3.client` object to interact with the bucket of the repository
+    Docs: https://boto3.amazonaws.com/v1/documentation/api/latest/index.html
+    """
     endpoint_url = repo_api.repo_bucket_api_url()
     import boto3
 
     client = boto3.client("s3", endpoint_url=endpoint_url, aws_access_key_id=token, aws_secret_access_key=token)
-    print(f"Client created. Use the name of the repo ({repo_api.repo_name}) as the name of the bucket")
+    log_message(f"Client created. Use the name of the repo ({repo_api.repo_name}) as the name of the bucket")
     return client
 
 
 def get_s3fs_client(repo_api: RepoAPI, token: str):
+    """
+    Creates an `s3fs.S3FileSystem` object to interact with the bucket of the repository
+    Docs: https://s3fs.readthedocs.io/en/latest/
+    """
     endpoint_url = repo_api.repo_bucket_api_url()
     import s3fs
 
     client = s3fs.S3FileSystem(endpoint_url=endpoint_url, key=token, secret=token)
-    print(f"Client created. Use the name of the repo ({repo_api.repo_name}) as the name of the bucket")
+    log_message(f"Client created. Use the name of the repo ({repo_api.repo_name}) as the name of the bucket")
     return client
 
 
