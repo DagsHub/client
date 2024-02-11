8efe172d929de0a68b281079841a31958c0ac356
sdafni <sdafni.yd@gmail.com>
2024-02-11 14:00:47 +0200

    import?

--- dagshub/data_engine/client/data_client.py
+++ dagshub/data_engine/client/data_client.py
@@ -24,7 +24,7 @@ from dagshub.data_engine.dtypes import MetadataFieldType
 from dagshub.data_engine.client.models import ScanOption
 from dagshub.data_engine.client.gql_mutations import GqlMutations
 from dagshub.data_engine.client.gql_queries import GqlQueries
-from dagshub.data_engine.model.datasource import Datasource, DatapointMetadataUpdateEntry, DatapointDeleteEntry
+from dagshub.data_engine.model.datasource import DatapointDeleteEntry
 from dagshub.data_engine.model.errors import DataEngineGqlError
 from dagshub.data_engine.model.query_result import QueryResult
 
