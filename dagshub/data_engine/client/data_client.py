dd88ac02acdd7c8761c00f533ac0c3b5d391c0f9
sdafni <sdafni.yd@gmail.com>
2024-02-18 14:10:33 +0200

    pep

--- dagshub/data_engine/client/data_client.py
+++ dagshub/data_engine/client/data_client.py
@@ -29,7 +29,8 @@ from dagshub.data_engine.model.query_result import QueryResult
 
 if TYPE_CHECKING:
     from dagshub.data_engine.datasources import DatasourceState
-    from dagshub.data_engine.model.datasource import Datasource, DatapointMetadataUpdateEntry, DatapointDeleteMetadataEntry
+    from dagshub.data_engine.model.datasource import Datasource, DatapointMetadataUpdateEntry,\
+        DatapointDeleteMetadataEntry
 
 logger = logging.getLogger(__name__)
 
