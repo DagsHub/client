81cfacc00fbf3a66817eb337811df37ecfd30eb3
KBolashev <kirill@dagshub.com>
2024-03-07 16:37:05 +0200

    flake pleasing

--- dagshub/common/__init__.py
+++ dagshub/common/__init__.py
@@ -5,3 +5,10 @@ from .notebook import is_inside_colab, is_inside_notebook
 force_terminal = False if is_inside_notebook() else None
 
 rich_console = rich.console.Console(force_terminal=force_terminal)
+
+__all__ = [
+    is_inside_colab.__name__,
+    is_inside_notebook.__name__,
+    "rich_console",
+    "force_terminal",
+]
