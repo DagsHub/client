d9d187f48f93207f8107a7d91520bfe273c04973
evgeni leonti <evgeni.leonti@gmail.com>
2024-07-01 15:52:41 +0300

    fix dda tests

--- tests/util.py
+++ tests/util.py
@@ -13,7 +13,10 @@ def remember_cwd():
 
 
 def valid_token_side_effect(request: httpx.Request) -> httpx.Response:
-    if request.headers["Authorization"] == "Bearer good-token":
+    valid_auth_tokens = ["good-token", "token-set-in-env-var", "token"]
+
+    auth_header = request.headers.get("Authorization")
+    if auth_header and auth_header.split(" ")[1] in valid_auth_tokens:
         return httpx.Response(
             200,
             json={
