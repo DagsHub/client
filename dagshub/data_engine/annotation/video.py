9fc5cd4b86921ef813d63a4c259a81411a1a65c7
Dean <dean@dagshub.com>
2026-04-13 14:23:59 +0300

    fix build_video_sequence_from_annotations

--- dagshub/data_engine/annotation/video.py
+++ dagshub/data_engine/annotation/video.py
@@ -2,27 +2,23 @@ from collections import defaultdict
 from typing import Optional, Sequence
 
 from dagshub_annotation_converter.ir.video import (
-    IRVideoBBoxFrameAnnotation,
+    IRVideoFrameAnnotationBase,
     IRVideoAnnotationTrack,
     IRVideoSequence,
 )
 
 
 def build_video_sequence_from_annotations(
-    annotations: Sequence[IRVideoBBoxFrameAnnotation],
+    annotations: Sequence[IRVideoFrameAnnotationBase],
     filename: Optional[str] = None,
 ) -> IRVideoSequence:
     # Pre-group annotations into tracks (required by new from_annotations API)
-    by_track: dict[str, list[IRVideoBBoxFrameAnnotation]] = defaultdict(list)
+    by_track: dict[str, list[IRVideoFrameAnnotationBase]] = defaultdict(list)
     for ann in annotations:
-        object_id = getattr(ann, "object_id", None)
-        if object_id is None and ann.__pydantic_extra__ is not None:
-            object_id = ann.__pydantic_extra__.get("object_id")
-        if object_id is None:
-            object_id = ann.imported_id
+        object_id = ann.imported_id
         if object_id is None:
             raise ValueError("Video annotation is missing an object identifier")
-        by_track[str(object_id)].append(ann)
+        by_track[object_id].append(ann)
 
     tracks = [
         IRVideoAnnotationTrack.from_annotations(anns, object_id=str(tid))
